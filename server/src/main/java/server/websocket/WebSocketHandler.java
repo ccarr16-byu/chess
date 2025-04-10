b39d66cd546335bcbbbddffd3cbc2657707ce233
Chance Carr <chancecarr1@gmail.com>
2025-04-09 18:58:58 -0600

    Create WebSocketHandler stubs

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -1,5 +1,38 @@
 package server.websocket;
 
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
+
+@WebSocket
 public class WebSocketHandler {
+    private final ConnectionHandler connections = new ConnectionHandler();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand userGameCommand = new Gson().fromJson(message, UserGameCommand.class);
+        switch (userGameCommand.getCommandType()) {
+            case CONNECT -> connect();
+            case MAKE_MOVE -> makeMove();
+            case LEAVE -> leave();
+            case RESIGN -> resign();
+        }
+    }
+
+    private void connect() {
+    }
+
+    private void makeMove() {
+    }
+
+    private void leave() {
+    }
+
+    private void resign() {
+    }
 }
